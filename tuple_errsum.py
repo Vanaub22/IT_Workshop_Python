d66051936d5ffaf961b92bc107dce17fe69329d8
Anuvab Chakravarty <106730134+Vanaub22@users.noreply.github.com>
2022-11-19 10:13:41 +0530

    Add files via upload

---
+++ tuple_errsum.py
@@ -0,0 +1,33 @@
+#to find the sum of two tuples using exception handling wherever necessary
+def isFloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+def isInteger(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
+def custom_sum(a,b):
+    if(isInteger(a) and isInteger(b)):
+        return(int(a)+int(b))
+    elif ((isFloat(a) and isInteger(b)) or (isFloat(b) and isInteger(a)) or (isFloat(a) and isFloat(b))):
+        return(float(a)+float(b))
+    else:
+        return(a+b)
+T1=tuple(input('Enter the elements of the first tuple separated by spaces:').split())
+T2=tuple(input('Enter the elements of the first tuple separated by spaces:').split())
+if(len(T1)>len(T2)):
+    T1,T2=T2,T1
+T3=[]
+for i in range(len(T2)):
+    try:
+        T3.append(custom_sum(T1[i],T2[i]))
+    except IndexError:
+        T3.append(T2[i])
+T3=tuple(T3)
+print('The resultant tuple is as follows:',T3)
+
